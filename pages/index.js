848fb75cd11937a844c0cf72532b9c5125f842ee
lz <985256910@qq.com>
2023-05-21 09:45:58 +0800

    chore:恢复之前页面

--- pages/index.js
+++ pages/index.js
@@ -67,23 +67,21 @@ export default function Index(props) {
     }
     return (
         <div className={styles.container}>
-            {/*<Head>*/}
-            {/*    <title>摩安保_摩托车保险_摩托车商业险</title>*/}
-            {/*    <meta name="description" content="提供在线办理摩托车保险业务，办理摩托车交强险，三者险和盗抢险"/>*/}
-            {/*    <meta name="keywords" content="如何购买摩托车保险,成都摩托车保险在哪里买,摩托车保险在哪里买,摩托车保险,摩托车交强险,摩托车三者险,摩托车盗抢险"/>*/}
-            {/*    <meta name="baidu-site-verification" content="code-BtSgOThgFs"/>*/}
-            {/*    <link rel="icon" href="/LOGO.png"/>*/}
-            {/*</Head>*/}
-            {/*<Insurance insuranceData={insuranceData}/>*/}
-            {/*{showSuccessPanel && <SuccessPanel/>}*/}
             <Head>
-                <title>个人兴趣爱好分享</title>
-                {/*<meta name="description" content="提供在线办理摩托车保险业务，办理摩托车交强险，三者险和盗抢险"/>*/}
-                {/*<meta name="keywords" content="如何购买摩托车保险,成都摩托车保险在哪里买,摩托车保险在哪里买,摩托车保险,摩托车交强险,摩托车三者险,摩托车盗抢险"/>*/}
+                <title>摩安保_摩托车保险_摩托车商业险</title>
+                <meta name="description" content="提供在线办理摩托车保险业务，办理摩托车交强险，三者险和盗抢险"/>
+                <meta name="keywords" content="如何购买摩托车保险,成都摩托车保险在哪里买,摩托车保险在哪里买,摩托车保险,摩托车交强险,摩托车三者险,摩托车盗抢险"/>
                 <meta name="baidu-site-verification" content="code-BtSgOThgFs"/>
                 <link rel="icon" href="/LOGO.png"/>
             </Head>
-            <Apply/>
+            <Insurance insuranceData={insuranceData}/>
+            {showSuccessPanel && <SuccessPanel/>}
+            {/*<Head>*/}
+            {/*    <title>个人兴趣爱好分享</title>*/}
+            {/*    <meta name="baidu-site-verification" content="code-BtSgOThgFs"/>*/}
+            {/*    <link rel="icon" href="/LOGO.png"/>*/}
+            {/*</Head>*/}
+            {/*<Apply/>*/}
         </div>
     )
 }
