66c5ae20500cdc4ababef05d919926850aba756c
lz <985256910@qq.com>
2023-06-27 15:35:20 +0800

    更换网页图标

--- pages/index.js
+++ pages/index.js
@@ -72,7 +72,7 @@ export default function Index(props) {
                 <meta name="description" content="提供在线办理摩托车保险业务，办理摩托车交强险，三者险和盗抢险"/>
                 <meta name="keywords" content="如何购买摩托车保险,成都摩托车保险在哪里买,摩托车保险在哪里买,摩托车保险,摩托车交强险,摩托车三者险,摩托车盗抢险"/>
                 <meta name="baidu-site-verification" content="code-BtSgOThgFs"/>
-                <link rel="icon" href="/LOGO.png"/>
+                <link rel="icon" href="/motosafe.svg"/>
             </Head>
             <Insurance insuranceData={insuranceData}/>
             {showSuccessPanel && <SuccessPanel/>}
