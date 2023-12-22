a554189a3789c6c902e2a860d9eb39f64ab8e62b
lz <985256910@qq.com>
2023-12-22 20:57:49 +0800

    version:2.0.0 修改head

--- pages/index.js
+++ pages/index.js
@@ -69,8 +69,8 @@ export default function Index(props) {
         <div className={styles.container}>
             <Head>
                 <title>摩安保_摩托车保险_摩托车商业险</title>
-                <meta name="description" content="提供在线办理摩托车保险业务，办理摩托车交强险，三者险和盗抢险"/>
-                <meta name="keywords" content="如何购买摩托车保险,成都摩托车保险在哪里买,摩托车保险在哪里买,摩托车保险,摩托车交强险,摩托车三者险,摩托车盗抢险"/>
+                <meta name="description" content="提供在线办理摩托车保险业务，提供专业的意见和建议！"/>
+                <meta name="keywords" content="如何购买摩托车保险,摩托车保险在哪里买,摩托车保险在哪里买,摩托车保险,摩托车交强险,摩托车三者险,摩托车盗抢险"/>
                 <meta name="baidu-site-verification" content="codeva-S1aaX4ALBY" />
                 <link rel="icon" href="/motosafe.svg"/>
             </Head>
